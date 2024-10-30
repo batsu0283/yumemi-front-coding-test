b6f7d4d31bf7d4e11309965a863e5494a082078b
batsu0283 <58724670+batsu0283@users.noreply.github.com>
2024-10-30 23:52:18 +0900

    vitestのcoverageの導入

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,19 @@ export default defineConfig({
   plugins: [react()],
   base: "/yumemi-front-coding-test/",
   test: {
+    include: ["src/**/*.test.{ts,tsx}", "src/**/*.spec.{ts,tsx}"], // テストファイルのみ対象
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/vitest.setup.ts",
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "src/**/*.d.ts",
+        "src/**/*.test.{ts,tsx}",
+        "src/**/*.spec.{ts,tsx}",
+      ],
+
+      reporter: ["text", "json", "html"],
+    },
   },
 });
